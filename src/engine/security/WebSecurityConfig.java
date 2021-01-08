25276763401b56aefad3c1388eab6823ac17b2d7
Knute Snortum <knute@snortum.net>
2021-01-08 10:07:16 -0800

    Improve session management

--- src/engine/security/WebSecurityConfig.java
+++ src/engine/security/WebSecurityConfig.java
@@ -1,12 +1,11 @@
 package engine.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -17,20 +16,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/api/register", "/actuator/shutdown", "/h2-console").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/api/quizzes/**").authenticated()
             .and()
                 .httpBasic()
             .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
                 .csrf().disable();
-    }
-
-    // TODO needed?
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("test@gmail.com")
-                .password("$2a$09$Vj95qAgNBEqzSPZKFhrvDu4pJbozPUGdv5KuhtwnIRe/K11YlKspG")
-                .roles("USER");
     }
 
     @Bean
